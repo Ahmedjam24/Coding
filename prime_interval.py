f2e3e97445f9aac96789c2e460e76e5294763ab4
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-01-31 16:33:35 +0530

    Update prime_interval.py

--- prime_interval.py
+++ prime_interval.py
@@ -2,11 +2,13 @@ usr1=raw_input()
 usr1=usr1.split()
 lower=int(usr1[0])
 upper=int(usr1[1])
+prime=[]
 for num in range(lower+1, upper):
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
                 break
         else:
-            print(num)
-            print " "
+            prime.extend(str(num))
+prime=' '.join(prime)
+print prime
