e2f96fba543df583e5db9512b63cfdb136703cf9
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-01-31 17:31:11 +0530

    Create armstrong_interval.py

---
+++ armstrong_interval.py
@@ -0,0 +1,15 @@
+usr1=raw_input()
+usr1=usr1.split()
+arm=[]
+for usr in range(int(usr1[0])+1,int(usr1[1])):
+    sum = 0
+    tem = usr
+    while usr > 0:
+        rem = usr % 10
+        sum = sum + (rem ** 3)
+        usr = usr / 10
+    if sum == tem:
+        arm.extend(str(tem))
+        arm.extend(' ')
+del arm[len(arm)-1]
+print arm
