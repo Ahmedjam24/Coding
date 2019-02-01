7c9ba687cee4e43ff41036211a29522dc05edaa8
Ahmedjam24 <30750847+Ahmedjam24@users.noreply.github.com>
2019-02-01 11:38:03 +0530

    Create arr_as_sort.py

---
+++ arr_as_sort.py
@@ -0,0 +1,16 @@
+usr_n=int(raw_input())
+usr_arr=raw_input()
+usr_arr=usr_arr.split()
+min=int(usr_arr[0])
+for i in range(0,usr_n):
+    for j in range (0,usr_n):
+        if(int(usr_arr[i])<int(usr_arr[j])):
+            t=usr_arr[i]
+            usr_arr[i]=usr_arr[j]
+            usr_arr[j]=t
+temp=0
+for i in range(0,usr_n):
+    if(temp!=0):
+        print(" ")
+    print usr_arr[i]
+    temp=1
